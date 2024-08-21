19d523a342ba1836ed483ac3a4aacb052203203d
VMoskov <vedran.moskov@fer.hr>
2024-08-21 20:32:02 +0200

    added get and post routes for teams

--- app/team/routes.py
+++ app/team/routes.py
@@ -1,6 +1,41 @@
+from os import name
 from app.team import bp
+from app.team.service import TeamService
+from flask import request, jsonify
+
+from app.team.team import Team
 
 
-@bp.route('/')
-def index():
-    return 'Welcome to the Team Manager!'
+@bp.route('/', methods=['GET'])
+def get_all():
+    team_service = TeamService()
+    teams = team_service.get()
+
+    serialized_teams = [team.to_dict() for team in teams]
+
+    return jsonify(serialized_teams), 200
+
+
+@bp.route('/<int:id>', methods=['GET'])
+def get_by_id(id):
+    team_service = TeamService()
+    team = team_service.get(id)
+
+    if team is None:
+        return 'Team not found', 404
+
+    serialized_team = team.to_dict()
+
+    return jsonify(serialized_team), 200
+
+
+@bp.route('/add', methods=['POST'])
+def add():
+    team = request.get_json()
+    team_service = TeamService()
+    
+    try:
+        team_service.add(team)
+        return 'Team added', 201
+    except ValueError as e:
+        return str(e), 400
--- app/team/team.py
+++ app/team/team.py
@@ -1,12 +1,20 @@
 from app.extensions import db
+from app.coach.coach import Coach
 
 
 class Team(db.Model):
     id = db.Column('team_id', db.Integer, primary_key=True)
-    name = db.Column('team_name', db.String(64))
+    name = db.Column('team_name', db.String(64), unique=True)
     home_stadium = db.Column(db.String(64))
-    coach_id = db.Column(db.Integer, db.ForeignKey('coach.id'))
-    coach = db.relationship('Coach', backref='teams', lazy='true')
+    coach_id = db.Column(db.Integer, db.ForeignKey('coach.coach_id'))
+    coach = db.relationship('Coach', backref='teams', lazy=True)
 
     def __repr__(self):
-        return f'<Team {self.name} from {self.city}, {self.country}>'
+        return f'<Team {self.name}>'
+    
+    def to_dict(self):
+        return {
+            'team_name': self.name,
+            'home_stadium': self.home_stadium,
+            'coach': self.coach.name if self.coach is not None else None
+        }

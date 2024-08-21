ac707e0ab3bc63006550bc8de3cefa515e8d558b
VMoskov <vedran.moskov@fer.hr>
2024-08-21 18:39:54 +0200

    modified sql script

--- database/clubmanager.sql
+++ database/clubmanager.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Coach IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Coach (
     coach_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -6,14 +6,14 @@ CREATE TABLE Coach IF NOT EXISTS (
     years_of_experience INTEGER
 );
 
-CREATE TABLE Team IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS  Team (
     team_id SERIAL PRIMARY KEY,
     team_name VARCHAR(50) UNIQUE NOT NULL,
     home_stadium VARCHAR(100),
     coach_id INTEGER REFERENCES Coach(coach_id)
 );
 
-CREATE TABLE Player IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Player (
     player_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -21,15 +21,15 @@ CREATE TABLE Player IF NOT EXISTS (
     dominant_foot VARCHAR(20)
 );
 
-CREATE TABLE PlayerTeam IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS PlayerTeam (
     player_team_id SERIAL PRIMARY KEY,
     player_position VARCHAR(50),
     player_id INTEGER REFERENCES Player(player_id),
     team_id INTEGER REFERENCES Team(team_id)
 );
 
-CREATE INDEX idx_player_id ON player_team(player_id);
-CREATE INDEX idx_team_id ON player_team(team_id);
+CREATE INDEX idx_player_id ON PlayerTeam(player_id);
+CREATE INDEX idx_team_id ON PlayerTeam(team_id);
 
 ALTER TABLE player
 ADD CONSTRAINT chk_dominant_foot CHECK (dominant_foot IN ('Left', 'Right', 'Both'));

27f30136c6af26f61a11878998914075b337e2f4
VMoskov <vedran.moskov@fer.hr>
2024-08-22 13:09:44 +0200

    added position entity to player team relation

--- app/player_team/player_team.py
+++ app/player_team/player_team.py
@@ -3,12 +3,13 @@ from app.extensions import db
 
 class PlayerTeam(db.Model):
     id = db.Column('player_team_id', db.Integer, primary_key=True)
-    player_position = db.Column(db.String(64))
     kit_number = db.Column(db.Integer)
     player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
     team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    position_id = db.Column(db.Integer, db.ForeignKey('position.id'))
     player = db.relationship('Player', backref='player_teams', lazy='true')
     team = db.relationship('Team', backref='player_teams', lazy='true')
+    position = db.relationship('Position', backref='player_teams', lazy='true')
 
     def __repr__(self):
         return f'<PlayerTeam {self.player_id} {self.team_id}>'

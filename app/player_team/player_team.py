d66d99ee2cbeba45a539b8694f4bea7e81975b2d
VMoskov <vedran.moskov@fer.hr>
2024-08-23 15:07:39 +0200

    added to_dict method and adapted model to database entity

--- app/player_team/player_team.py
+++ app/player_team/player_team.py
@@ -2,14 +2,25 @@ from app.extensions import db
 
 
 class PlayerTeam(db.Model):
+    __tablename__ = 'playerteam'
+
     id = db.Column('player_team_id', db.Integer, primary_key=True)
     kit_number = db.Column(db.Integer)
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
-    position_id = db.Column(db.Integer, db.ForeignKey('position.id'))
-    player = db.relationship('Player', backref='player_teams', lazy='true')
-    team = db.relationship('Team', backref='player_teams', lazy='true')
-    position = db.relationship('Position', backref='player_teams', lazy='true')
+    player_id = db.Column(db.Integer, db.ForeignKey('player.player_id'))
+    team_id = db.Column(db.Integer, db.ForeignKey('team.team_id'))
+    position_id = db.Column(db.Integer, db.ForeignKey('position.position_id'))
+    player = db.relationship('Player', backref='player_teams', lazy=True)
+    team = db.relationship('Team', backref='player_teams', lazy=True)
+    position = db.relationship('Position', backref='player_teams', lazy=True)
 
     def __repr__(self):
         return f'<PlayerTeam {self.player_id} {self.team_id}>'
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'kit_number': self.kit_number,
+            'player_id': self.player_id,
+            'team_id': self.team_id,
+            'position_id': self.position_id
+        }

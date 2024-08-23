e54033eb0942e4fc592b68d79628f588d63c93a4
VMoskov <vedran.moskov@fer.hr>
2024-08-23 15:08:19 +0200

    added route for assigning team to a player

--- app/player_team/routes.py
+++ app/player_team/routes.py
@@ -1,6 +1,21 @@
 from app.player_team import bp
+from app.player_team.service import PlayerTeamService
+from flask import request
 
 
 @bp.route('/')
 def index():
     return 'Welcome to the Player Team Manager!'
+
+
+@bp.route('/assign/<int:player_id>/<int:team_id>', methods=['POST'])
+def assign_player_to_team(player_id, team_id):
+    body = request.get_json()
+    player_team_service = PlayerTeamService()
+
+    try:
+        player_team = player_team_service.assign_player_to_team(player_id, team_id, body)
+        return 'Player assigned to the team', 201
+    except ValueError as e:
+        return str(e), 400
+    

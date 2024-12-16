884fd4f867e6fe7d8aa9eeffcb92fbf296a573c8
VMoskov <vedran.moskov@fer.hr>
2024-12-16 18:01:15 +0100

    add jwt manager configuration

--- config.py
+++ config.py
@@ -4,7 +4,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super secret key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
         or 'postgresql://admin:admin@localhost:5433/clubmanager'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super secret jwt key'
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
+    JWT_REFRESH_TOKEN_EXPIRES = 604800
+    JWT_TOKEN_LOCATION = ['headers']
+    SWAGGER = {
+        'title': 'Club Manager API',
+        'uiversion': 3
+    }

6dd1b13c4fc88d84301c5c167ce8df9936463144
VMoskov <vedran.moskov@fer.hr>
2024-12-16 18:00:22 +0100

    added jwt secret key export

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY="your secret key"
 export DATABASE_URI="postgresql://admin:admin@localhost:5433/clubmanager"
+export JWT_SECRET_KEY="your jwt secret key"
 
 export FLASK_APP=app
 export FLASK_ENV=development 

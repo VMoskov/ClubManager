8f29ee608d5dda080d85861317e37b8e9d99c878
VMoskov <vedran.moskov@fer.hr>
2024-12-18 00:14:57 +0100

    added position route documentation

--- app/position/routes.py
+++ app/position/routes.py
@@ -7,6 +7,34 @@ from app.utils import roles_required
 @bp.route('/')
 @roles_required(['admin', 'user'])
 def get_all():
+    '''
+    Get all positions
+    ---
+    tags:
+        - position
+    operationId: get_all_positions
+    responses:
+        200:
+            description: A list of all positions
+            schema:
+                type: array
+                items:
+                    type: object
+                    properties:
+                        id:
+                            type: integer
+                        name:
+                            type: string
+                        description:
+                            type: string
+                        salary:
+                            type: integer
+            examples:
+                application/json: [{id: 1, name: 'Software Engineer', description: 'Develop software', salary: 100000}, 
+                                   {id: 2, name: 'Data Scientist', description: 'Analyze data', salary: 120000}]
+        403:
+            description: insufficient permissions
+    '''
     position_service = PositionService()
     positions = position_service.get()
 
